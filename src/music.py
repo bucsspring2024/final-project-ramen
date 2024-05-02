11858d35a8e6dd2a19fb2ac177bbbda9b5f94cbd
andyBhuang <ahuang16@binghamton.edu>
2024-05-01 21:47:48 -0400

    yay

--- src/music.py
+++ src/music.py
@@ -4,4 +4,5 @@ class Music:
     def startmusic(self):
         pygame.mixer.init()
         pygame.mixer.music.load("assets/music.wav")
+        pygame.mixer.music.set_volume(0.3)
         pygame.mixer.music.play(-1)
